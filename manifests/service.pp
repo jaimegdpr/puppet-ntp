2555514840ff8e1c2dcae0437e129f212c49c4fb
Jaime Gomez <jaimegdpr@users.noreply.github.com>
2017-09-07 11:32:35 +0200

    Added ntpdate service | Disabling chronyd on Centos7

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,6 +3,26 @@
 class ntp::service inherits ntp {
 
   if $ntp::service_manage == true {
+
+    if ($osfamily == 'Redhat') and ($operatingsystemmajrelease == '7') {
+        service { 'chronyd':
+          ensure     => stopped,
+          enable     => false,
+          hasstatus  => true,
+          hasrestart => true,
+          before => Service['ntpdate'],
+        }
+    }
+
+    service { 'ntpdate':
+      ensure     => $ntp::service_ensure,
+      enable     => $ntp::service_enable,
+      provider   => $ntp::service_provider,
+      hasstatus  => true,
+      hasrestart => true,
+      before => Service['ntpd'],
+    }
+
     service { 'ntp':
       ensure     => $ntp::service_ensure,
       enable     => $ntp::service_enable,
